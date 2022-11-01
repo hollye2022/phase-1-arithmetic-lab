061b7751be1881d259241a64c17b4c16e665f9c1
chengying <hollye2558@gmail.com>
2022-11-01 14:59:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 =2
+const multiply = num1 * num2 ;
+let num3 = 12
+let num4 = 3
+const mod = num3 / num4 ;
+const random = Math.ceil(Math.random()) + 1
+const max = Math.max(1,3,20,19) ;
+
